2d85af6017be48822920b5c2150177376cb5e4f0
abdurrahman6489 <abdurrahman489@gmail.com>
2023-03-20 00:14:37 +0530

    pushing the code

---
+++ Arrays/container_with_most_water.java
@@ -0,0 +1,7 @@
+class container_with_most_water{
+    public static void main(String[] args){
+        int height[] = {1,8,6,2,5,4,8,3,7};
+        int n = height.length;
+        System.out.print(Array.toString());
+    }
+}
